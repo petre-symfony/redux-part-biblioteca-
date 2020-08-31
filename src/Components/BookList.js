e9d2aa2a29baf0df2715aa1c27e076eb15dfad23
petrero <petrero@gmail.com>
2020-08-31 09:49:33 +0300

    Reveal details related to books in the list of books

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,15 +1,91 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/BookList.css';
 
 class BookList extends React.Component {
   render(){
     const { books } = this.props;
 
     return (
-      <div>
-        <ul>
-          {books && books.map(book => <li key={book.id}>{book.name}</li>)}
-        </ul>
-      </div>
+      <React.Fragment>
+          {books && books.map(book => (
+            <div className="card m-2 book">
+              <p>{book.name}</p>
+              {book.subject && <p>{book.subject}</p>}
+              <div className="card-body d-flex flex-column content">
+                <div className="book-location-editors-details-container">
+                  <div className="card book-location-editors-container">
+                    { book.location && (
+                      <div className="card book-location">
+                        <p>Locatie</p>
+                        <div className="card-body">
+                          <p>{book.location}</p>
+                        </div>
+                      </div>
+                    )}
+                    {book.publishers && (
+                      <div className="card book-editors">
+                        <p>Editori</p>
+                        <div className="card-body">
+                          <ul className="list-group list-group-flush">
+                            {book.publishers.map(publisher => (
+                              <li className="list-group-item">{publisher.Name}</li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
+                    )}
+                    { book.Content ?
+                      <a href="#" className="btn btn-success">Vezi detalii de continut</a> :
+                      <div className="btn btn-primary">Nu are detalii de continut</div>
+                    }
+                  </div>
+                  <div className="card book-details">
+                    <p>Detalii</p>
+                    <div className="card-body">
+                      <ul className="list-group">
+                        {book.firstPublishedYear &&
+                          <li className="list-group-item">Prima aparitie: {book.firstPublishedYear}</li>
+                        }
+                        {book.bookPublishedYear &&
+                          <li className="list-group-item">Carte publicata in anul: {book.bookPublishedYear}</li>
+                        }
+                        {book.volume &&
+                          <li className="list-group-item">Volum: {book.volume} --- {book.complete ? ' Avem toate volumele' : ' Nu avem toate volumele'}</li>
+                        }
+                        {book.pages &&
+                        <li className="list-group-item">{book.pages} pagini</li>
+                        }
+                        {book.format &&
+                          <li className="list-group-item">Format: {book.format}</li>
+                        }
+                        {book.ISBN &&
+                          <li className="list-group-item">ISBN: {book.ISBN}</li>
+                        }
+                        {book.other &&
+                          <li className="list-group-item">{book.other}</li>
+                        }
+                        {book.language &&
+                          <li className="list-group-item">Scrisa in limba {book.language}</li>
+                        }
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                {book.authors && (
+                  <div className="card book-authors">
+                    <p>Authors</p>
+                    <div className="card-body">
+                      <ul className="list-group">
+                        {book.authors.map(author => <li className="list-group-item">{author.fullName}</li>)}
+                      </ul>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          ))}
+      </React.Fragment>
     );
   }
 }
